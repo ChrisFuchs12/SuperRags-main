ab55f061e7ef7fdcd7330a7334d1d57ab7626be9
ChrisFuchs12 <157928174+ChrisFuchs12@users.noreply.github.com>
2024-10-04 11:43:45 +0100

    a

--- Assets/Scripts/Giu/BringUpMenu.cs
+++ Assets/Scripts/Giu/BringUpMenu.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class BringUpMenu : MonoBehaviour
 {
@@ -43,4 +44,9 @@ public class BringUpMenu : MonoBehaviour
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Locked;
     }
+
+    public void PlayGame(){
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+
 }
